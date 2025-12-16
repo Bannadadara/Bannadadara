39cfbbb0dcf875849e901ffb5fbd3d15f34ec7b7
Bannadadara <bannada.dara@gmail.com>
2025-12-16 18:02:27 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "16px", background: "#222", color: "#fff" }}>
+      © 2024 Bannada Daara
+    </footer>
+  );
+}
