e761311da41e3f2fac1b7291fd49420d521dbe81
Bannadadara <bannada.dara@gmail.com>
2025-12-16 18:05:26 +0530

    Create About.jsx

---
+++ src/sections/About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <section style={{ padding: "20px" }}>
+      <h2>About Us</h2>
+      <p>
+        Bannada Daara is a home-grown initiative started in 2020, creating
+        hand-stitched products using upcycled textile excesses.
+      </p>
+    </section>
+  );
+}
