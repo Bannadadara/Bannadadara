061b12cbe8e62f4f3ccc9a4cd04a80fba0714e1d
Bannadadara <bannada.dara@gmail.com>
2025-12-16 18:06:49 +0530

    Create Contact.jsx

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <section style={{ padding: "20px", textAlign: "center" }}>
+      <h2>Contact</h2>
+      <p>📞 81057 50221</p>
+      <p>📷 @bannada_daara_21</p>
+      <p>✉️ lavanyajyothigudde@gmail.com</p>
+    </section>
+  );
+}
