c3eccc6c2c73d949e19ac29d02748c71adc6385f
Bannadadara <bannada.dara@gmail.com>
2025-12-17 23:09:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const products = [
     { name: "Reversible Tote Bag", price: "Rs. 500/-", category: "Bags", img: "images/reversible-tote2.jpg" },
     { name: "Box Tote (Plain)", price: "Rs. 350/-", category: "Bags", img: "images/box-plain.jpg" },
     { name: "Box Tote (Patch-work)", price: "Rs. 500/-", category: "Bags", img: "images/box-patch.jpg" },
-   - { name: "Laptop Bag (50-50)", price: "Rs. 800/-", category: "Bags", img: "images/laptop.jpg" },
+    { name: "Laptop Bag (50-50)", price: "Rs. 800/-", category: "Bags", img: "images/laptop.jpg" },
     { name: "Sling Bag (Regular)", price: "Rs. 250/-", category: "Bags", img: "images/sling-reg.jpg" },
     { name: "Sling Bag (Medium)", price: "Rs. 450/-", category: "Bags", img: "images/sling-reg-m.jpg" },
     { name: "Sling Bag (Patch-work L)", price: "Rs. 550/-", category: "Bags", img: "images/sling-patch-l.jpg" },
@@ -19,31 +19,30 @@ const products = [
     { name: "Pad-Holder (folded)", price: "Rs. 100/-", category: "Pouches", img: "images/pad-holder-folded.jpg" },
     { name: "Box Pouch", price: "Rs. 170/-", category: "Pouches", img: "images/box-pouch.jpg" },
     { name: "Flat Pouch", price: "Rs. 80/-", category: "Pouches", img: "images/flat-pouch.jpg" },
-    { name: "TRINKET", price: "Rs. 40/-", category: "Pouches", img: "images/TRINKET.jpg" }
+    { name: "TRINKET", price: "Rs. 40/-", category: "Pouches", img: "images/TRINKET.jpg" },
     
     // STATIONERY
     { name: "A4 Files", price: "Rs. 200/-", category: "Stationery", img: "images/files.jpg" },
     { name: "Book (Embroidered Cover)", price: "Rs. 450/-", category: "Stationery", img: "images/book.jpg" },
-    { name: "pen pouch", price: "Rs. 100/-", category: "Stationery", img: "images/pouch.jpg" }
+    { name: "Pen Pouch", price: "Rs. 100/-", category: "Stationery", img: "images/pouch.jpg" },
     
     // ACCESSORIES & DECOR
     { name: "Cutlery Kit", price: "Rs. 260/-", category: "Accessories", img: "images/cutlery.jpg" },
     { name: "Cloth Mask", price: "Rs. 50/-", category: "Accessories", img: "images/mask.jpg" },
-    { name: "Patch-work Quilt", price: "On Request", category: "Decor", img: "images/quilt.jpg" }
+    { name: "Patch-work Quilt", price: "On Request", category: "Decor", img: "images/quilt.jpg" },
     { name: "Patch-work Table cloth", price: "On Request", category: "Decor", img: "images/tcloth.jpg" }
 ];
 
 const productList = document.getElementById('product-list');
 
-products.forEach(item => {
-    productList.innerHTML += `
-        <div class="card">
-            <img src="${item.img}" alt="${item.name}">
-            <div class="card-info">
-                <span class="tag">${item.category}</span>
-                <h3>${item.name}</h3>
-                <p class="price">${item.price}</p>
-            </div>
+// Using a template literal with map and join is more efficient for larger lists
+productList.innerHTML = products.map(item => `
+    <div class="card">
+        <img src="${item.img}" alt="${item.name}">
+        <div class="card-info">
+            <span class="tag">${item.category}</span>
+            <h3>${item.name}</h3>
+            <p class="price">${item.price}</p>
         </div>
-    `;
-});
+    </div>
+`).join('');
